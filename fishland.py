462668952979bd7c8a59170745b034fb7b0a5672
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-14 10:05:38 +0300

    Update fishland.py

--- fishland.py
+++ fishland.py
@@ -1,4 +1,4 @@
-skumria_cena = float(input())
+price_of_mackerel = float(input())
 caca_cena = float(input())
 midi_cena = 7.5
 palamud_cena = 0
@@ -8,7 +8,7 @@ palamud_kg = float(input())
 safrid_kg = float(input())
 midi_kg = int(input())
 
-palamud_cena = skumria_cena + (skumria_cena * 0.6)
+palamud_cena = price_of_mackerel + (price_of_mackerel * 0.6)
 sum_palamud = palamud_kg * palamud_cena
 safrid_cena = caca_cena + (caca_cena * 0.8)
 sum_safrid = safrid_kg * safrid_cena
