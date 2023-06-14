ce1af3b904828e66057ef114d95d47e0118ba9f1
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-14 10:07:45 +0300

    Update fishland.py

--- fishland.py
+++ fishland.py
@@ -1,6 +1,6 @@
 price_of_mackerel = float(input())
 price_of_sprats = float(input())
-midi_cena = 7.5
+price_of_mussels = 7.5
 palamud_cena = 0
 safrid_cena = 0
 
@@ -12,7 +12,7 @@ palamud_cena = price_of_mackerel + (price_of_mackerel * 0.6)
 sum_palamud = palamud_kg * palamud_cena
 safrid_cena = price_of_sprats + (price_of_sprats * 0.8)
 sum_safrid = safrid_kg * safrid_cena
-sum_midi = midi_kg * midi_cena
+sum_midi = midi_kg * price_of_mussels
 
 total = sum_palamud + sum_safrid + sum_midi
 print(f"{total:.2f}")
