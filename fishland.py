74b9b14826dc4a13981d4651d8bacdc101de4247
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-14 10:03:22 +0300

    Add files via upload

---
+++ fishland.py
@@ -0,0 +1,18 @@
+skumria_cena = float(input())
+caca_cena = float(input())
+midi_cena = 7.5
+palamud_cena = 0
+safrid_cena = 0
+
+palamud_kg = float(input())
+safrid_kg = float(input())
+midi_kg = int(input())
+
+palamud_cena = skumria_cena + (skumria_cena * 0.6)
+sum_palamud = palamud_kg * palamud_cena
+safrid_cena = caca_cena + (caca_cena * 0.8)
+sum_safrid = safrid_kg * safrid_cena
+sum_midi = midi_kg * midi_cena
+
+total = sum_palamud + sum_safrid + sum_midi
+print(f"{total:.2f}")
