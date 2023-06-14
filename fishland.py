60b0255246e33bc54c5451a36f08dbc9d51a399b
Galin Mihaylov <101407321+GalinMih87@users.noreply.github.com>
2023-06-14 10:18:24 +0300

    Update fishland.py

--- fishland.py
+++ fishland.py
@@ -6,13 +6,13 @@ price_of_safrid = 0
 
 weight_of_bonito = float(input())
 weight_of_safrid = float(input())
-midi_kg = int(input())
+weight_of_mussels = int(input())
 
 bonito_price = price_of_mackerel + (price_of_mackerel * 0.6)
 final_price_of_bonito = weight of bonito * bonito_price
 price_of_safrid = price_of_sprats + (price_of_sprats * 0.8)
 final_price_of_safrid = weight_of_safrid * price_of_safrid
-sum_midi = midi_kg * price_of_mussels
+sum_midi = weight_of_mussels * price_of_mussels
 
 total = final_price_of_bonito + final_price_of_safrid + sum_midi
 print(f"{total:.2f}")
